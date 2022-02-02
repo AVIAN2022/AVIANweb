37ea311ecd5b4c52afbeb5fa990260636131692c
avian2022 <avian.gbshs@gmail.com>
2022-02-02 14:08:53 +0900

    찐찐찐막

--- app.js
+++ app.js
@@ -1,55 +1,73 @@
-if (matchMedia("screen and (min-width: 1500px)").matches) {
-    const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
-const text = intro.querySelector("h1");
-//END SECTION
-const section = document.querySelector("section");
-const end = section.querySelector("h1");
 
-//SCROLLMAGIC
-const controller = new ScrollMagic.Controller();
+$(function() {
+  const html = document.documentElement;
+  const canvas = document.getElementById("intro"); //캔버스 id 불러와야함
+  const context = canvas.getContext("2d");
+const intro = document.getElementById("intro1").documentElement;
 
-//Scenes
-let scene = new ScrollMagic
-    .Scene(
-        {duration: 5000, triggerElement: intro, triggerHook: 0}
-    )
-    .setPin(intro)
-    .addTo(controller);
+  const frameCount = 94; //프레임 숫자 교정하세요
+  
+  const currentFrame = index => (
+      `프레임셋/${index.toString().padStart(4, '0')}.png` //경로 수정 , $구문은 남겨야함.
 
-//Video Animation
-let accelamount = 0.1;
-let scrollpos = 0;
-let delay = 0;
+  )
 
-scene.on("update", e => {
-    scrollpos = e.scrollPos / 1500;
-});
+  const preloadImages = () => {
+      for(let i = 1; i< frameCount; i++){
+          const img = new Image();
+          img.src = currentFrame(i)
+      }
+  };
+  // Create, load and draw the image
+  const img = new Image()
+  img.src = currentFrame(1); // we'll make this dynamic in the next step, for now we'll just load image 1 of our sequence
+  // Set canvas dimensions
+  canvas.width=1920;
+  canvas.height=1080;
+  
+  img.onload=function(){
+      context.drawImage(img, 0, 0);
+  }
 
-setInterval(() => {
-    delay += (scrollpos - delay)*accelamount;
-
-    video.currentTime = delay;
-}, 33.3);
-(function (document) {
-    const markers = [...document.querySelectorAll('mark')];
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach((entry) => {
-            if (entry.intersectionRatio > 0) {
-                entry.target.style.animationPlayState = 'running';
-                observer.unobserve(entry.target);
-            }
-        });
-    }, {threshold: 0});
-
-    markers.forEach(mark => {
-        observer.observe(mark);
-    });
-})(document);
+  const updateImage = index => {
+      img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
 }
 
 
+  window.addEventListener('scroll', () => {
+      const scrollTop = html.scrollTop; //scrollTop은 현재 수직위치
+      const maxScrollTop = $("#n_1920__2").offset().top+80; //문서 스크롤 높이 - 창 높이
+      const scrollFraction = scrollTop / maxScrollTop; //현재 수직위치가 약 몇 %인지?
+      const frameIndex = Math.min(
+          frameCount - 1,
+          Math.floor(scrollFraction * frameCount)
+      );
+
+
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
+});
+
+  preloadImages();
+});
+(function (document) {
+    const markers = [...document.querySelectorAll('mark')];
+    
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.style.animationPlayState = 'running';
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.8
+    });
+    
+    markers.forEach(mark => {
+      observer.observe(mark);
+    });
+  })(document);
 ScrollReveal().reveal('.n_15_', {
     distance: '40%',
     origin: 'bottom',
@@ -79,6 +97,9 @@ ScrollReveal().reveal('.innerslider', {
     origin: 'bottom',
     duraiton: 500
 });
+function middlego(){}
+function leftgo(){}
+function rightgo(){}
 let innerSlider = document.querySelector(".innerslider");
 function middlego(){
     innerSlider.style.left = '-840px';
